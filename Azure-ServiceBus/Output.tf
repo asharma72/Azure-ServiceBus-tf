a63c466b53d20047248a1cbaed299e937a463988
asharma72 <ag.sharma23@gmail.com>
2020-02-06 19:57:12 +0530

    Update Output.tf

--- Azure-ServiceBus/Output.tf
+++ Azure-ServiceBus/Output.tf
@@ -1,7 +1,7 @@
-output "Namespace Connection String" {
+output "Namespace_Connection_String" {
   value = "${azurerm_servicebus_namespace.example.default_primary_connection_string}"
 }
 
-output "Shared Access Policy PrimaryKey" {
+output "Shared_Access_Policy_PrimaryKey" {
   value = "${azurerm_servicebus_namespace.example.default_primary_key}"
 }
